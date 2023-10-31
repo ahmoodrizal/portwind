690c8c38b566f9e77d173ede8fff24fd9bdf3d70
Ahmad Rizal <amdrizalc@gmail.com>
2023-10-31 16:28:39 +0700

    init

---
+++ dist/js/script.js
@@ -0,0 +1,19 @@
+// Navar Fixed
+window.onscroll = function () {
+  const header = document.querySelector('header');
+  const fixNavbar = header.offsetTop;
+  if (window.pageYOffset > fixNavbar) {
+    header.classList.add('navbar-fixed');
+  } else {
+    header.classList.remove('navbar-fixed');
+  }
+};
+
+// Hamburger
+const hamburger = document.querySelector('#hamburger');
+const navMenu = document.querySelector('#nav-menu');
+
+hamburger.addEventListener('click', function () {
+  hamburger.classList.toggle('hamburger-active');
+  navMenu.classList.toggle('hidden');
+});
